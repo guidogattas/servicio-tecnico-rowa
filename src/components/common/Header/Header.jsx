1af17052958f5b30a34f77004b6e5eaa8645c714
Guido Gattas <guido.gattas@gmail.com>
2025-03-14 17:37:06 -0300

    [TEST] Test casa

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -7,16 +7,16 @@ const Header = () => {
         <>
             <section className='flex flex-col md:flex-row justify-around  md:py-4 animated-bg text-customWhite font-dm font-bold text-[24px]'>
                 <div className='flex justify-center'>
-                    <Link to="/"> 
-                    <img className='md:max-h-[100px]' src="\images\Logo.png" alt="Logo Tu Service Ya" />
+                    <Link to="/">
+                        <img className='md:max-h-[100px]' src="\images\Logo.png" alt="Logo Tu Service Ya" />
                     </Link>
                     <img className='hidden md:block ml-4 md:max-h-[80px]' src="\images\bombas_rowa-new.png" alt="Logo Tu Service Ya" />
                 </div>
-                <NavBar />  
+                <NavBar />
             </section>
-                {/* <div className='flex items-center gap-3 text-3xl bg-bgWhatsAppBar items-center justify-center'>
-                    <WhatsApp showText={true} />
-                </div> */}
+            {/* <div className='flex items-center justify-center gap-3 text-3xl bg-bgWhatsAppBar'>
+                <WhatsApp showText={true} />
+            </div> */}
             <a
                 className="fixed z-50 transition-transform duration-300 bottom-4 right-6 md:bottom-4 md:right-10 md:transform animate-bounce hover:scale-110"
                 target="_blank"
@@ -28,4 +28,4 @@ const Header = () => {
     );
 };
 
- export default Header;
+export default Header;
